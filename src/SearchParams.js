b66802dbc4965af2d4d1a6e9f5632df746bdd9fa
Farrukh Pashazadeh <farrukhpashazadeh@gmail.com>
2021-04-22 18:22:19 +0400

    changed to JSX

---
+++ src/SearchParams.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchParams = () => {
+    const location = "Seattle, WA";
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    <input 
+                    id="location"
+                    value={location}
+                    placeholder="location"
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
